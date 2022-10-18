1cb1bd8f76a8462ada84af0ddca2d9f1c6e624c6
sohivansh <sohivansh@gmail.com>
2022-10-18 13:18:14 +0530

    added avatar and some text

--- app.py
+++ app.py
@@ -7,8 +7,17 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def layout():
-    return render_template("layout.html")
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/work")
+def work():
+    return render_template("work.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
